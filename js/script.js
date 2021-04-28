f869533d3f6ab176aef7779ec8f101b4983d6a5c
Danila <klimovdanila13@gmail.com>
2021-04-28 07:33:18 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,57 @@
+"use strict";
+/*
+//Объект «Человек», задаем значения
+const human = {
+    Name: "Alex",
+    Age: 27,
+   isMarried: false,
+};
+
+//Объект «Человек», выводим данные
+console.log(human.Age);
+
+Массив - частный случай объекта (спец. объект). Пример:
+let aderf = ['main.png', 'poll.jpg', 'car.gif'];
+console.log(aderf[2]);
+
+alert('Hello!');
+const result = confirm ('Are you here?');
+console.log(result);
+
+const answer = prompt('Are you 18 age?', '18');
+console.log(answer + 100);
+
+const answers = [];
+
+answers[0] = prompt('First name');
+answers[1] = prompt('Last name');
+answers[2] = prompt('Age');
+
+console.log(typeof(answers));
+
+
+const category = 'toys';
+const baby = 'huos';
+console.log(`https://someurl.com/${category}/5/${baby}`);
+
+const user = 'Oleg';
+alert(`Hi, ${user}`);
+
+
+
+let baba = 10,
+    gaga = 10;
+
+++baba;
+--gaga;
+
+console.log(baba);
+console.log(gaga);
+
+*/
+
+const isChecked = false,
+      isClose = false;
+console.log(isChecked && !isClose);
+
+
